74594be3d2eb5c3cd46f4ae9f85870ead15ad34f
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2022-02-04 01:18:22 +0530

    BST included

---
+++ 1. Array, searching & sorting/leadersInArray.cpp
@@ -0,0 +1,25 @@
+
+// Leader -- an element is a leader only if there is no element greater that that element on it's right side.
+
+vector<int> leaders(vector<int> &nums)
+{
+    if (nums.size() == 0)
+    {
+        return {};
+    }
+    int n = nums.size();
+    vector<int> result;
+    int currentLeader = nums[n - 1];
+    result.push_back(currentLeader);
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (currentLeader < nums[i])
+        {
+            currentLeader = nums[i];
+            result.push_back(currentLeader);
+        }
+    }
+
+    return result;
+}
