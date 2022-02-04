c620dfb55e3857a7dd581693414f149c6b929c77
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2022-02-05 00:37:58 +0530

    BST included

---
+++ 1. Array, searching & sorting/searching/firstOccurance.cpp
@@ -0,0 +1,35 @@
+
+
+// return the 1st occurance of target element
+
+int firstOccurance(vector<int> &nums, int target)
+{
+    int start = 0, end = nums.size();
+
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
+
+        if (nums[mid] > target)
+        {
+            end = mid - 1;
+        }
+        else if (nums[mid] < target)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            if (mid == 0 || nums[mid] != nums[mid - 1])
+            {
+                return mid;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
