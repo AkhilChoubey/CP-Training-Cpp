296bdde6c2be874e2a56be28b6a12ecd204f3000
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2022-02-20 02:20:29 +0530

    Mathematics

--- 1. Array, searching & sorting/searching/searchInSortedRotatedArray.cpp
+++ 1. Array, searching & sorting/searching/searchInSortedRotatedArray.cpp
@@ -1,6 +1,6 @@
-// we will use binary search, idea is ine half will always be sorted , we will compare the mid element
+// we will use binary search, idea is one half will always be sorted , we will compare the mid element
 // with one of the corner elment , let's say first element, if(nums[mid] > nums[start]) then left half is sorted
-// else right half is sorted. if element lie in sorted half we will search there ot we will appy
+// else right half is sorted. if element lie in sorted half we will search there or we will apply
 // search in unsorted part.
 
 int seachInRotatedArray(vector<int> &nums, int target)
