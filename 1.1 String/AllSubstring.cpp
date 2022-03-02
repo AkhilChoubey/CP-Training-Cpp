0af78097f5a6c7fd7f51ff3ff9f768f8897bd5ac
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2022-03-02 15:05:29 +0530

    CP

--- 1.1 String/AllSubstring.cpp
+++ 1.1 String/AllSubstring.cpp
@@ -1,10 +1,13 @@
 
+// Substring -- A Substring takes out characters from a string placed between 
+//              two specified indices in a continuous order
+
 vector<string> AllSubstrings(string &s) {
     int n = s.size();
 
     vector<string> result;
 
-    for(int len = 1; len < s.size(); len++) {
+    for(int len = 1; len < n; len++) {
         for(int i = 0; i + len <= n; i++) {
             string curr = s.substr(i, len);  // s.substr(i, len) will create substring from 1 to len
             result.push_back(curr);
