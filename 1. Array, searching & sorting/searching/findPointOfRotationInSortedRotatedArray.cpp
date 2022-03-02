0a01d49a5f7d6bf5581630ccab9c3665209c0398
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2022-03-02 12:43:31 +0530

    CP

---
+++ 1. Array, searching & sorting/searching/findPointOfRotationInSortedRotatedArray.cpp
@@ -0,0 +1,33 @@
+
+// Logic -- If the element is smaller than the last,
+//              the point of rotation definitely lies in the left of the middle element
+//              Hence, redefine the range for probing [L .. M – 1]
+//          Else
+//              The point of rotation certainly lies in the right of the middle element
+//              Hence, redefine the range for probing [M+1, L]
+
+
+int pointOfRotation(vector<int> arr) {
+    int start = 0;
+    int end = arr.size();
+
+    int last = arr[end-1];
+
+    int mid = (start + end) / 2;
+
+    while(start <= end) {
+        mid = (start + end) / 2;
+
+        if(arr[i] < last) {
+            end = mid - 1;
+        }
+        else if(arr[i] > last) {
+            start = mid + 1;
+        }
+        else {
+            break;
+        }
+    }
+
+    return mid;
+}
